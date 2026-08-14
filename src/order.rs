e5a2fbcfa0e81b6c3c914276ae0369ea53398097
g1g1g1g1g1 <89553030+g1g1g1g1g1@users.noreply.github.com>
2026-08-13 23:11:32 -0400

    Initial set up for the order book!

---
+++ src/order.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Side {
+    Buy,
+    Sell,
+}
+
+#[derive(Debug, Clone)]
+pub struct Order {
+    pub id: u64,
+    pub side: Side,
+    pub price: u64,
+    pub quantity: u64,
+}
+
+impl Order {
+    pub fn new(id: u64, side: Side, price: u64, quantity: u64) -> Self {
+        Self {
+            id,
+            side,
+            price,
+            quantity,
+        }
+    }
+}
