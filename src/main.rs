e2def51e0e294479b207b2c94c62a10f6177204b
g1g1g1g1g1 <89553030+g1g1g1g1g1@users.noreply.github.com>
2026-08-15 16:19:18 -0400

    Change main.rs to have starting template

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,6 @@ use order::{Order, Side};
 use order_book::{OrderBook};
 
 fn main() {
-    let buy = Order::new(1, Side::Buy, 100, 10);
-    let buy2 = Order::new(2, Side::Buy, 90, 5);
-    let buy3 = Order::new(3, Side::Buy, 90, 50);
-
-    let sell = Order::new(4, Side::Sell, 50, 10);
-    let sell2 = Order::new(5, Side::Sell, 90, 4);
-    let sell3 = Order::new(6, Side::Sell, 50, 3);
-
     let mut orderbook = OrderBook::new();
-
-    orderbook.add_order(buy);
-    orderbook.add_order(buy2);
-    orderbook.add_order(buy3);
-    
-    orderbook.add_order(sell);
-    orderbook.add_order(sell2);
-    orderbook.add_order(sell3);
-
-    println!{"{:?}", orderbook};
+    // Do work.
 }
